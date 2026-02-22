83745139f22ee59d22fad0a9b4948814e0ceab18
NEERAD <neerad_2024bcse079@nitsri.ac.in>
2026-02-22 15:36:35 +0530

    Creating Student table with hibernate

--- src/main/java/com/neerad/Main.java
+++ src/main/java/com/neerad/Main.java
@@ -1,10 +1,25 @@
 package com.neerad;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
         student.setId(101);
         student.setName("Neerad");
         student.setTech("Java");
+
+        // saving student object with hibernate
+
+        Configuration config = new Configuration().configure();
+        config.addAnnotatedClass(com.neerad.Student.class);
+        SessionFactory factory = config.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(student);
+        tx.commit();
     }
 }
--- src/main/java/com/neerad/Student.java
+++ src/main/java/com/neerad/Student.java
@@ -1,40 +1,15 @@
 package com.neerad;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+@Entity
+@Data
 public class Student {
+    @Id
     private String name;
     private int id;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", tech='" + tech + '\'' +
-                '}';
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setTech(String tech) {
-        this.tech = tech;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTech() {
-        return tech;
-    }
-
     private String tech;
+
 }
